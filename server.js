169398652fd0845c04f55931fd26f869f6a906e1
zeropse <129523087+zeropse@users.noreply.github.com>
2024-12-14 19:35:38 +0530

    serv

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "dist")));
-
+// Serve the index.html file directly for all requests
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.resolve("index.html"));
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
